9db351270ea25f1edf9f63d60b426861c897a0fc
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-24 16:29:05 +0800

    add feedback

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,4 +1,5 @@
 export const getPoetry = async() => {
+    // TODO feedback:可以把url抽出来
     const response =  await fetch("https://v1.jinrishici.com/all.json");
     const data = await response.json();
     return [data.origin, data.author, data.content]
